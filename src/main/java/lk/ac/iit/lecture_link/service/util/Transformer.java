a638ebe9c8fb608c13aab25cb02f4e6be0a8b20f
tharindu <chanaka152@gmail.com>
2025-01-24 11:16:25 +0530

    [LL-250124] - Rename Transformer

--- src/main/java/lk/ac/iit/lecture_link/service/util/Transformer.java
+++ src/main/java/lk/ac/iit/lecture_link/service/util/Transformer.java
@@ -9,48 +9,37 @@ import lk.ac.iit.lecture_link.entity.Lecturer;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Component
 public class Transformer {
 
-    private final ModelMapper mapper;
+    private final ModelMapper modelMapper;
 
-    public Transformer(ModelMapper mapper) {
-        this.mapper = mapper;
-
-//        mapper.typeMap(MultipartFile.class, Picture.class)
-//                .setConverter(ctx -> null);
+    public Transformer(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
     }
 
     public Lecturer fromLecturerReqDto(LecturerReqDto lecturerReqDto){
-        return mapper.map(lecturerReqDto, Lecturer.class);
+        return modelMapper.map(lecturerReqDto, Lecturer.class);
     }
 
     public Lecturer fromLecturerDto(LecturerDto lecturerDto){
-        return mapper.map(lecturerDto, Lecturer.class);
+        return modelMapper.map(lecturerDto, Lecturer.class);
     }
 
     public LecturerDto toLecturerDto(Lecturer lecturer){
-        return mapper.map(lecturer, LecturerDto.class);
+        return modelMapper.map(lecturer, LecturerDto.class);
     }
 
     public Institute fromInstituteReqDto(InstituteReqDto instituteReqDto){
-        return mapper.map(instituteReqDto, Institute.class);
+        return modelMapper.map(instituteReqDto, Institute.class);
     }
 
     public Institute fromInstituteDto(InstituteDto instituteDto){
-        return mapper.map(instituteDto, Institute.class);
+        return modelMapper.map(instituteDto, Institute.class);
     }
 
     public InstituteDto toInstituteDto(Institute institute){
-        return mapper.map(institute, InstituteDto.class);
+        return modelMapper.map(institute, InstituteDto.class);
     }
-
-    public List<LecturerDto> toLectureDtoList(List<Lecturer> lecturerList){
-        return lecturerList.stream().map(this::toLecturerDto).collect(Collectors.toList());
-    }
-
 
 }
