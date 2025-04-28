ce225d59be4e704f95f639256ffeeff4207ad01f
tharindu <chanaka152@gmail.com>
2025-04-28 19:46:31 +0530

    [LL-250427] - Refactor filters

--- src/main/java/lk/ac/iit/lecture_link/repository/LecturerRepository.java
+++ src/main/java/lk/ac/iit/lecture_link/repository/LecturerRepository.java
@@ -16,13 +16,13 @@ public interface LecturerRepository extends JpaRepository<Lecturer, Long> {
     @Query("SELECT DISTINCT l FROM Lecturer l " +
             "LEFT JOIN l.qualifications q " +
             "WHERE (:division IS NULL OR LOWER(l.division) LIKE LOWER(CONCAT('%', :division, '%'))) " +
-            "OR (:payRateLower IS NULL OR :payRateUpper IS NULL OR (l.hourlyPayRate BETWEEN :payRateLower AND :payRateUpper)) " +
-            "OR (:qualification IS NULL OR LOWER(q.level) LIKE LOWER(CONCAT('%', :qualification, '%'))) " +
-            "OR (:isAssigned IS NULL OR l.isAssigned = :isAssigned) " +
-            "OR (:language IS NULL OR FUNCTION('FIND_IN_SET', :language, l.language) > 0) " +
-            "OR (:globalSearch IS NULL OR LOWER(l.name) LIKE LOWER(CONCAT('%', :globalSearch, '%')) " +
-            "OR LOWER(l.preference) LIKE LOWER(CONCAT('%', :globalSearch, '%')) " +
-            "OR LOWER(l.fieldOfWork) LIKE LOWER(CONCAT('%', :globalSearch, '%')))")
+            "AND (:payRateLower IS NULL OR :payRateUpper IS NULL OR (l.hourlyPayRate BETWEEN :payRateLower AND :payRateUpper)) " +
+            "AND (:qualification IS NULL OR LOWER(q.level) LIKE LOWER(CONCAT('%', :qualification, '%'))) " +
+            "AND (:isAssigned IS NULL OR l.isAssigned = :isAssigned) " +
+            "AND (:language IS NULL OR FUNCTION('FIND_IN_SET', :language, l.language) > 0) " +
+            "AND (:globalSearch IS NULL OR LOWER(l.name) LIKE LOWER(CONCAT('%', :globalSearch, '%')) " +
+            "AND LOWER(l.preference) LIKE LOWER(CONCAT('%', :globalSearch, '%')) " +
+            "AND LOWER(l.fieldOfWork) LIKE LOWER(CONCAT('%', :globalSearch, '%')))")
     Page<Lecturer> findFilteredLecturers(
             @Param("division") String division,
             @Param("payRateLower") BigDecimal payRateLower,
